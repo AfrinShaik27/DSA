2e0df117a8f15dc19908e579eae1c4a3a105a866
AfrinShaik27 <shaikafrin750@gmail.ocm>
2026-09-16 16:43:28 +0530

    Initial Commit..

--- src/com/learning/Eg1.java
+++ src/com/learning/Eg1.java
@@ -3,7 +3,31 @@ package com.learning;
 public class Eg1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(args);
+//        System.out.println(args);
+//        short a=128;
+//        System.out.println(a);
+//        int b=100_00_00;
+//        System.out.println(b);
+//        char c='A';
+//        System.out.println(c);
+//        boolean d=false;
+//        System.out.println(d);
+//        float e=20.89f;
+//        System.out.println(e);
+//        int aa=199;
+//        System.out.println(aa);
+//        long f=12267878379898l;
+//        System.out.println("long value is: "+f);
+//        double g=2679.89d;
+//        System.out.println(g);
+//        float h=23.0f;
+//        System.out.println(h);
+//        int i=257;
+//        byte j=(byte) (a);
+//        System.out.println(j);
+
+
+
 
     }
 }
